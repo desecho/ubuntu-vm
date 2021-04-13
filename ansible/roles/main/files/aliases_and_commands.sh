f0738c6f92e3d05fd899768ccd61a4b6a2fd626f
Anton Samarchyan <desecho@gmail.com>
2021-04-12 22:07:18 -0400

    Changes

--- ansible/roles/main/files/aliases_and_commands.sh
+++ ansible/roles/main/files/aliases_and_commands.sh
@@ -1,5 +1,15 @@
-alias gitamend='git add . && git commit --amend --no-edit'
 function dockerti(){
-    docker -ti $1 bash
+    docker exec -ti $1 sh
 }
+
+function getsecret(){
+    kubectl get secret $1 -o json | jq -r ".data.$2" | base64 -d
+}
+
+function kubectlti(){
+    kubectl exec -ti $1 -- sh
+}
+
+alias gitamend='git add . && git commit --amend --no-edit'
 alias fastcommit='git add . && git commit -m"Changes"'
+alias fastpush='fastcommit && git push'
