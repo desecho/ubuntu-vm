24760252b5efb0d9aa38586d1c94659b489f9784
Anton Samarchyan <desecho@gmail.com>
2021-09-16 21:18:16 -0400

    Changes

--- ansible/roles/main/files/aliases_and_commands.sh
+++ ansible/roles/main/files/aliases_and_commands.sh
@@ -1,15 +1,24 @@
 # shellcheck disable=SC2148
 
-function dockersh(){
-    docker exec -ti "$1" sh
-}
+# Custom names are for `getsecret`, `fastpush`, `fastcommit`.
+# These are the names which don't start with the command they are calling.
 
+# Kubectl
 function getsecret(){
     kubectl get secret "$1" -o json | jq -r ".data.$2" | base64 -d
 }
 
-function kubectlsh(){
+function kti(){
     kubectl exec -ti "$1" -- sh
+}
+
+alias k=kubectl
+
+#------------------------------------------------------------------------------
+
+# Docker
+function dockerti(){
+    docker exec -ti "$1" sh
 }
 
 function dockerclean() {
@@ -17,22 +26,43 @@ function dockerclean() {
     docker volume rm "$(docker volume ls --filter dangling=true -q)"
 }
 
-function gitrebase() {
-    branch=$(git branch --show-current)
-    git checkout master
-    git pull
-    git checkout "$branch"
-    git rebase master
+#------------------------------------------------------------------------------
+# Git
+
+function _get_default_branch() {
+    git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'
 }
 
-function gitupdate() {
-    git checkout master
+function grebase() {
+    current_branch=$(git branch --show-current)
+    default_branch=$(_get_default_branch)
+    git switch "$default_branch"
+    git pull
+    git switch "$current_branch"
+    git rebase "$default_branch"
+}
+
+function gmaster() {
+    git switch "$(_get_default_branch)"
     git pull
 }
 
-alias gitamend='git add . && git commit --amend --no-edit'
-alias fastcommit='git add . && git commit -m"Changes"'
-alias fastpush='fastcommit && git push'
+alias g="git"
+alias gl='git log --oneline --decorate'
+alias gs="git status"
+alias gb="git branch"
+alias gsw="git switch"
+alias gd="git diff"
+alias gamend="git add . && git commit --amend --no-edit"
+alias gfp="git push --force-with-lease"
+alias gnew="git switch -c"
+alias gpull="git pull"
+alias fastcommit="git add . && git commit -m'Changes'"
+alias fastpush="fastcommit && git push"
+
+#------------------------------------------------------------------------------
+# Replacements
 alias ls=exa
 alias cat=batcat
-alias k=kubectl
+alias ocat=/usr/bin/cat
+#------------------------------------------------------------------------------
