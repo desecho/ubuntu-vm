485913df818cc6bb0e63a963e28e2eeee7a31379
Anton Samarchyan <desecho@gmail.com>
2021-10-02 17:26:58 -0400

    Changes

--- install_oh_my_zsh.sh
+++ install_oh_my_zsh.sh
@@ -4,7 +4,7 @@ set -eou pipefail
 
 SHELL_INIT_FILE="${HOME}/.zshrc"
 PLUGINS="git docker docker-compose thefuck alias-finder systemd aliases vscode ansible screen terraform helm kubectl colored-man-pages npm man python command-not-found sudo zsh_reload gitfast node gitignore pip golang"
-CUSTOM_PLUGINS="django doctl"
+CUSTOM_PLUGINS="doctl"
 
 sudo apt install zsh curl -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
